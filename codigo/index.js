4f3687843dcdc503fab8c54fbd0634b7682d9b67
ndavidrincon <digitalonly.agency@gmail.com>
2022-10-14 18:33:15 -0500

    Crear eventos de botones

---
+++ codigo/index.js
@@ -0,0 +1,13 @@
+let botonCrearUsuario = document.getElementById("boton-crear")
+botonCrearUsuario.addEventListener("click", opcionDeEntrada)
+let botonIngresarUsuario =document.getElementById("boton-ingresar")
+botonIngresarUsuario.addEventListener("click", opcionDeEntrada)
+
+function opcionDeEntrada (){
+    if (botonCrearUsuario = true) {
+        alert("Crear usuario se obturo")
+    }
+    else if (botonIngresarUsuario = true) {
+        alert("Ingresar se obturo")
+    }
+}
